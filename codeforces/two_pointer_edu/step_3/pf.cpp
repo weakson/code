b7ddcea59822e18bbfc9cd153ed6aa2eb3ed1ad3
weakson <950823eason@gmail.com>
2024-09-05 23:28:20 +0800

    remove test.cpp

---
+++ codeforces/two_pointer_edu/step_3/pf.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define F first
+#define S second
+#define weakson ios::sync_with_stdio(0), cin.tie(0);
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define dbg(x) cout << #x << " = " << x << endl;
+using namespace std;
+
+int main(){
+	weakson;
+
+    int n, m;
+    cin >> n >> m;
+
+    string s, t;
+    cin >> s >> t;
+
+    map <char, int> mp;
+    for (auto i : t){
+        mp[i]++;
+    }
+
+    int l = 0, r = 0;
+
+    while (l < n){
+        while (r < n && 
+    }
+}
