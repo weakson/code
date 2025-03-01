c15e9d556ddc784c95d20d27859172d184507380
weakson <950823eason@gmail.com>
2025-03-01 08:43:36 +0800

    2025/3/1 08:43

--- ntoj/toj536.cpp
+++ ntoj/toj536.cpp
@@ -1,15 +1,19 @@
-#include<iostream>
+#include<bits/stdc++.h>
+#define ll long long
+#define F first
+#define S second
+#define weakson ios::sync_with_stdio(0), cin.tie(0);
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define swap(a,b) a ^= b ^= a ^= b
+#define dbg(x) cout << #x << " = " << x << endl;
 using namespace std;
 
 int main(){
-	ios::sync_with_stdio(0), cin.tie(0);
-	int a, b, c, d;
-	cin >> a >> b >> c >> d;
-	if(a > d || b < c){
-		cout << "no\n";
-	}
-	else{
-		cout << "yes\n";
-	}
-	return 0;
+    weakson;
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (c < b && d > a) cout << "yes\n";
+    else cout << "no\n";
 }
